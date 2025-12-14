cb51ec1f571ee3fbd9fc77cb48f133f1a1b2e35f
dev-lnk <lkivanmail@gmail.com>
2025-12-14 18:47:41 +0300

    fix: many fixes

--- docker/config/postgres/init/create_database.sql
+++ docker/config/postgres/init/create_database.sql
@@ -1,1 +1,4 @@
+-- Create testing database
 CREATE DATABASE my_database_test;
+-- Grant all privileges to postgres user on both databases
+GRANT ALL PRIVILEGES ON DATABASE my_database_test TO postgres;
