84b0ce14171afaf14be9a4e411e6c196b548ccf1
dev-lnk <lkivanmail@gmail.com>
2025-12-14 19:12:03 +0300

    fix: many fixes

--- docker/config/php/entrypoint-prod.sh
+++ docker/config/php/entrypoint-prod.sh
@@ -23,6 +23,7 @@ php artisan optimize
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+php artisan storage:link
 
 # Start supervisor to run Octane
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
